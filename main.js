70ced52f11e7b85a26f7a0cb9c360f98e1c85b68
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-10-31 11:23:15 -0600

    aertaesdgth

---
+++ main.js
@@ -0,0 +1,73 @@
+// // IPO QUESTION #2 - Mad Lib 1//
+
+// // Input
+// let name1 = prompt("Enter a friend's name:");
+// let color = prompt("Enter a color:");
+
+// // Process
+// let msg = `My friend ${name1} likes ${color} bananas.`;
+
+// // Output
+// alert(msg);
+
+//
+//
+//
+//
+//
+//
+//
+
+// //IPO QUESTION #3 - Mad Lib 2
+
+// // Input
+// let sub = prompt("Enter a school subject:");
+// let color = prompt("Enter a color:");
+// let noun = prompt("Enter a noun:");
+
+// // Process
+// let msg = `In ${sub} class, our teacher was wearing a ${color} ${noun}.`;
+
+// // Output
+// alert(msg);
+
+//
+//
+//
+//
+//
+//
+//
+
+// //IPO QUESTION #4 - Fahrenheit to Celsius
+
+// // Input
+// let temp = +prompt("Enter temperature in degrees Fahrenheit:");
+
+// // Process
+// let cal = ((+temp - 32) * 5) / 9;
+// let msg = `${temp} degrees Fahrenheit is ${cal} degrees Celsius.`;
+
+// // Output
+// alert(msg);
+
+//
+//
+//
+//
+//
+//
+//
+
+//IPO QUESTION #5 - Tip & Total Bill
+
+//Input
+
+let bill = +prompt("Enter starting bill amount($)");
+let tip = +prompt("Enter percentage to tip");
+
+// Process
+let total = bill + tip;
+
+// Output
+alert(`$${bill} plus a tip of $${tip} = $${total}`);
